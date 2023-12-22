73eef189e905d8b70889ec0cbaab5f22561ba04f
rm-8 <mohamedridhwan8@gmail.com>
2023-12-22 14:48:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function moveDodgerLeft() {
+    let left = dodger.style.left;
+    left = parseInt(left);
+  
+    dodger.style.left = `${left - 5}px`;
+  }
+  
+  function moveDodgerRight() {
+    let left = dodger.style.left;
+    left = parseInt(left);
+  
+    dodger.style.left = `${left + 5}px`;
+  }
